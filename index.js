a1f33092fb661203205665c95f67c43d11779330
Jasper Mayone <jasper@jaspermayone.com>
2022-03-27 20:38:50 -0400

    :zap: Works on enabling httos

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import fs from "fs";
+import https from "https";
 import express from "express";
+import actuator from "express-actuator";
+// import createLightship from "lightship";
 import bodyParser from "body-parser";
+import compression from "compression";
+
 import chalk from "chalk";
 import "dotenv/config";
 
@@ -10,10 +16,13 @@ import apiRoutes from "./routes/api.js";
 
 import { handle404, logErrors, handleErrors } from "./handleErrors.js";
 
-const app = express();
+// const lightship = createLightship();
 const PORT = process.env.PORT;
+const app = express();
 
 app.use(bodyParser.json());
+app.use(compression());
+app.use(actuator());
 
 app.get("/", (req, res) => {
   res.redirect("/api");
@@ -22,13 +31,36 @@ app.get("/", (req, res) => {
 app.use("/checkout", checkoutRoutes);
 app.use("/webhook", webhookRoutes);
 app.use("/usage", usageRoutes);
+// app.use('/files', express.static(path.join(__dirname, 'files')))
 app.use("/api", apiRoutes);
-
 
 app.use(handle404);
 app.use(logErrors);
 app.use(handleErrors);
 
-app.listen(PORT, () => {
-  console.log(chalk.green(`Server started on http://localhost:${PORT}`));
-});
+const server = https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(PORT, function () {
+    // lightship.signalReady();
+    console.log(chalk.green(`Server started on http://localhost:${PORT}`));
+  });
+
+/*const server = https
+.createServer(
+  {
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cert"),
+  },
+  app
+)*/
+
+//const server = app.listen(PORT, () => {
+// lightship.signalReady();
+//console.log(chalk.green(`Server started on http://localhost:${PORT}`));
+//});
