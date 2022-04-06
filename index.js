d38a0e0eff1d3eea621ff8c905d9466d7d218efb
Jasper Mayone <jasper@jaspermayone.com>
2022-04-05 20:06:05 -0400

    :rocket: Docker work

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ import bodyParser from "body-parser";
 import compression from "compression";
 
 import chalk from "chalk";
-import winston from "winston";
-import "dotenv/config";
+//import winston from "winston";
+import mongoose from "mongoose";
+import "dotenv/config"
 
 import checkoutRoutes from "./routes/checkout.js";
 import webhookRoutes from "./routes/webhook.js";
 import usageRoutes from "./routes/usage.js";
 import apiRoutes from "./routes/api.js";
 
-import { handle404, logErrors, handleErrors } from "./handleErrors.js";
-
 //const lightship = createLightship();
 const PORT = process.env.PORT;
+const HOST = process.env.HOST;
 const app = express();
 
+app.use(express.json());
 app.use(bodyParser.json());
 app.use(compression());
-app.use(actuator())
-
+app.use(actuator());
 
 app.get("/", (req, res) => {
   res.redirect("/api");
@@ -35,12 +35,29 @@ app.use("/usage", usageRoutes);
 // app.use('/files', express.static(path.join(__dirname, 'files')))
 app.use("/api", apiRoutes);
 
-app.use(handle404);
 app.use(logErrors);
 app.use(handleErrors);
 
-const server = app.listen(PORT, () => {
+mongoose.connect(process.env.MONGODB_URI_REMOTE, {
+  useNewUrlParser: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.error(chalk.red(error));
+});
+
+db.once("open", () => {
+  console.log(
+    chalk.green(
+      "API Connected to MongoDB Instance at: " +
+        chalk.blue(process.env.MONGODB_URI_REMOTE)
+    )
+  );
+});
+
+const server = app.listen(PORT, HOST, () => {
   // lightship.signalReady();
- // console.log(chalk.green(`Server started on http://localhost:${PORT}`));
-  logger.ready(`Server started on http://localhost:${PORT}`);
+  console.log(chalk.green(`Server started on http://${HOST}:${PORT}`));
 });
