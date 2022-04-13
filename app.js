92e20b5085ca726754f0380d700409accf945099
Jasper Mayone <jasper@jaspermayone.com>
2022-04-13 18:24:46 -0400

    :wastebasket: Cleanup from stripe testing

--- app.js
+++ app.js
@@ -4,15 +4,8 @@ import bodyParser from "body-parser";
 import compression from "compression";
 import helmet from "helmet";
 import cors from 'cors';
-
-import chalk from "chalk";
-//import winston from "winston";
-import mongoose from "mongoose";
 import "dotenv/config";
 
-import webhookRoute from "./routes/webhook.js";
-import checkoutRoute from "./routes/checkout.js";
-import usageRoute from "./routes/usage.js";
 import apiRoute from "./routes/api.js";
 import adminRoutes from "./routes/admin.js";
 import loginRoute from "./routes/login.js";
@@ -30,9 +23,6 @@ app.get("/", (req, res) => {
   res.redirect("/api");
 });
 
-app.use("/webhook", webhookRoute);
-app.use("/checkout", checkoutRoute);
-app.use("/usage", usageRoute);
 // app.use('/files', express.stat ic(path.join(__dirname, 'files')))
 app.use("/api", apiRoute);
 app.use("/admin", adminRoutes);
