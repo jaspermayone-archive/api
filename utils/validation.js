772ca1a196fad6b7a1ed78135005e9a6a1ad0183
Jasper Mayone <jasper@jaspermayone.com>
2022-04-17 17:51:49 -0400

    :ambulance: Fix with name validation

--- utils/validation.js
+++ utils/validation.js
@@ -2,10 +2,10 @@ import Joi from "joi";
 
 export const registerValidation = (data) => {
   const schema = Joi.object({
-    name: Joi.string().alphanum().min(3).max(30).required(),
+    name: Joi.string().required(),
 
     password: Joi.string()
-      .pattern(new RegExp("^[a-zA-Z0-9]{3,30}$"))
+      .pattern()
       .required(),
 
     email: Joi.string()
