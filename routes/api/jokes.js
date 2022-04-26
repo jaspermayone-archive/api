a8820e951db54d3d16b3110a7bd2103bd1fb9098
Jasper Mayone <jasper@jaspermayone.com>
2022-04-25 21:34:49 -0400

    :bug: Fixes another nasty bug

--- routes/api/jokes.js
+++ routes/api/jokes.js
@@ -1,25 +1,15 @@
 import express from "express";
-import mongoose from "mongoose";
-
 const router = express.Router();
+
+import Joke from "../../models/Jokes.js";
 
 router.get("/", (req, res) => {
   res.redirect("/api/v0/jokes/random");
 });
 
 router.get("/random", async (req, res) => {
-
-  const Connection = await mongoose.connection;
-
-  Connection.collection('jokes').aggregate([
-    { $sample: { size: 1 } }
-  ]).toArray(function (err, result) {
-    if (err) return res.status(500).send(err);
-    res.json({
-      id: result[0]._id,
-      joke: result[0].joke,
-    });
-  })
+  const targetRecord = await Joke.aggregate([{ $sample: { size: 1 } }]);
+  res.send(targetRecord[0]);
 });
 
 export default router;
--- routes/api/quotes.js
+++ routes/api/quotes.js
@@ -1,27 +1,16 @@
 import express from "express";
-import mongoose from "mongoose";
-
 const router = express.Router();
+
+import Quote from "../../models/Quotes.js";
 
 router.get("/", (req, res) => {
   res.redirect("/api/v0/quotes/random");
 });
 
 router.get("/random", async (req, res) => {
-
-  const Connection = await mongoose.connection;
-
-  Connection.collection('quotes').aggregate([
-    { $sample: { size: 1 } }
-  ]).toArray(function (err, result) {
-    if (err) return res.status(500).send(err);
-
-    res.json({
-      id: result._id,
-      quote: result.quote,
-      author: result.author,
-    });
-  })
+  const targetRecord = await Quote.aggregate([{ $sample: { size: 1 } }]);
+  res.send(targetRecord[0]);
 });
+
 
 export default router;
