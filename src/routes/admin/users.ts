afe3dcd27dff7526327132f5d8acf956dec7ed03
Jasper Mayone <jasper@jaspermayone.com>
2022-05-03 07:23:38 -0400

    :memo: Docs fixes

--- src/routes/admin/users.ts
+++ src/routes/admin/users.ts
@@ -9,8 +9,34 @@ import { registerValidation } from "../../utils/validation";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /admin/users/{id}:
+ *    get:
+ *      tags:
+ *        - /admin
+ *      summary: Fetch a user by id
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: Numeric ID of the user to fetch
+ *          schema:
+ *            type: integer
+ *            required: true
+ *      produces: application/json
+ *      responses:
+ *        200:
+ *          description: Successful Response
+ *          schema:
+ *            $ref: '#/definitions/Joke'
+ *        401:
+ *          description: Unauthorized (No token provided)
+ *        500:
+ *          description: Internal Server Error
+ */
 router.get("/:id", (req, res) => {
-  User.findById(req.params.id, (err, user) => {
+  const id = req.params.id;
+  User.findById(id, (err, user) => {
     if (err) {
       res.status(500).send(err);
     } else {
@@ -19,23 +45,67 @@ router.get("/:id", (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * /admin/users/add:
+ *    post:
+ *      tags:
+ *        - /admin
+ *      summary: Add a user
+ *      parameters:
+ *        - in: header
+ *          name: user
+ *          description: User to add
+ *          required: true
+ *          schema:
+ *            $ref: '#/definitions/User'
+ *      produces: application/json
+ *      responses:
+ *        200:
+ *          description: Successful Response
+ *          schema:
+ *            type: "object"
+ *            properties:
+ *              _id:
+ *                type: string
+ *              user:
+ *                type: string
+ *              dateUploaded:
+ *                type: string
+ *                format: date
+ *        400:
+ *          description: Bad Request
+ *          schema:
+ *            type: string
+ *            example: "User already exists in system!"
+ *        401:
+ *          description: Unauthorized (No token provided)
+ *        500:
+ *          description: Internal Server Error
+ */
 router.post("/add", async (req, res) => {
   const { error } = registerValidation(req.body);
   if (error) return res.status(401).send(error.details[0].message);
 
-  const emailExists = await User.findOne({ email: req.body.email });
+  const email = req.body.email;
+  const password = req.body.password;
+  const first_name = req.body.first_name;
+  const last_name = req.body.last_name;
+  const accountType = req.body.accountType;
+
+  const emailExists = await User.findOne({ email: email });
   if (emailExists)
     return res.status(400).send("Email already exists in system!");
 
-  const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   const user = new User({
     _id: uuidv4(),
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email: req.body.email,
+    first_name: first_name,
+    last_name: last_name,
+    email: email,
     password: hashedPassword,
-    accountType: req.body.accountType,
+    accountType: accountType,
   });
 
   try {
@@ -54,8 +124,38 @@ router.post("/add", async (req, res) => {
   }
 });
 
+/**
+ * @swagger
+ * /admin/users/{id}:
+ *    put:
+ *      tags:
+ *        - /admin
+ *      summary: Find a user by id and update it
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: Numeric ID of the user to find and update
+ *          schema:
+ *            type: integer
+ *            required: true
+ *        - in: header
+ *          name: user
+ *          schema:
+ *            type: string
+ *      produces: application/json
+ *      responses:
+ *        200:
+ *          description: Successful Response
+ *          schema:
+ *            $ref: "#/definitions/User"
+ *        401:
+ *          description: Unauthorized (No token provided)
+ *        500:
+ *          description: Internal Server Error
+ */
 router.put("/:id", (req, res) => {
-  User.findByIdAndUpdate(req.params.id, req.body, (err, user) => {
+  const id = req.params.id;
+  User.findByIdAndUpdate(id, req.body, (err, user) => {
     if (err) {
       res.status(500).send(err);
     } else {
@@ -64,8 +164,34 @@ router.put("/:id", (req, res) => {
   });
 });
 
+/**
+ * @swagger
+ * /admin/users/{id}:
+ *    delete:
+ *      tags:
+ *        - /admin
+ *      summary: Find a user by id and delete it
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          description: Numeric ID of the user to find and delete
+ *          schema:
+ *            type: integer
+ *            required: true
+ *      produces: application/json
+ *      responses:
+ *        200:
+ *          description: Successful Response
+ *          schema:
+ *            $ref: '#/definitions/User'
+ *        401:
+ *          description: Unauthorized (No token provided)
+ *        500:
+ *          description: Internal Server Error
+ */
 router.delete("/:id", (req, res) => {
-  User.findByIdAndDelete(req.params.id, (err, user) => {
+  const id = req.params.id;
+  User.findByIdAndDelete(id, (err, user) => {
     if (err) {
       res.status(500).send(err);
     } else {
