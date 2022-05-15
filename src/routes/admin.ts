6243db90109ad0c153a682abf2e4b3c7ba038c20
Jasper Mayone <jasper@jaspermayone.com>
2022-05-15 16:37:34 -0400

    :recycle: Add back in error endpoint to admin routes

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,5 +1,8 @@
 import healthCheck from "@nymdev/health-check";
 import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
+import errorLogger from "../logger";
 
 import jokeRoutes from "./admin/jokes";
 import qotdRoutes from "./admin/qotd";
@@ -16,5 +19,16 @@ router.use("/jokes", jokeRoutes);
 router.use("/quotes", quoteRoutes);
 router.use("/qotd", qotdRoutes);
 router.use("/restart", restartRoute);
+
+router.get("/error", (req, res) => {
+    try {
+        // code that will throw an error
+        throw new Error("Test error from admin route");
+      } catch (error) {
+        const errorID = uuidv4();
+        errorLogger(error, errorID);
+        res.status(500).send({ error: `${error}`, errorID: `${errorID}` });
+      }
+});
 
 export default router;
