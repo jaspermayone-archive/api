e6b6e618e6b82b39c3c104c4e2caa8ce5eede867
Jasper Mayone <jasper@jaspermayone.com>
2022-08-27 21:18:54 -0400

    :bug: Bugfix with docs

--- src/routes/api/qotd.ts
+++ src/routes/api/qotd.ts
@@ -5,10 +5,47 @@ import Qotd from "../../models/Qotd";
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /v4/qotd:
+ *    get:
+ *      tags:
+ *        - /v4
+ *      summary: gets a question of the day
+ *      responses:
+ *        302:
+ *          description: Found (Redirect to /random)
+ *        401:
+ *          description: Unauthorized (No token provided)
+ */
 router.get("/", (req, res) => {
   res.redirect("/v4/qotd/random");
 });
 
+/**
+ * @swagger
+ * /v4/qotd/random:
+ *    get:
+ *      tags:
+ *        - /v4
+ *      summary: Fetch a random question of the day
+ *      produces: application/json
+ *      responses:
+ *        200:
+ *          description: Successful Response
+ *          schema:
+ *            type: "object"
+ *            properties:
+ *              id:
+ *                type: string
+ *              qo:
+ *                type: string
+ *              uniqueID:
+ *                type: string
+ *                format: date
+ *        401:
+ *          description: Unauthorized (No token provided)
+ */
 router.get("/random", async (req, res) => {
   const targetRecord = await Qotd.aggregate([{ $sample: { size: 1 } }]);
   res.send(targetRecord[0]);
