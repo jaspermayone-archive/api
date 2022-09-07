ef46ab05ad9d6988a3bde32e436cb86e5ca81ab6
Jasper Mayone <jasper@jaspermayone.com>
2022-09-07 07:37:20 -0400

    :ambulance: Hotfix

--- src/app.ts
+++ src/app.ts
@@ -25,9 +25,10 @@ const bruteforce = new ExpressBrute(store, {
   freeRetries: 5,
   minWait: 5 * 60 * 1000, // 5 minutes
   maxWait: 60 * 60 * 1000, // 1 hour
-  failCallback: (req, res) => {
+  failCallback: (req, res, nextValidRequestDate) => {
     res.status(429).json({
       error: "Too many requests, please try again later.",
+      nextValidRequestDate: nextValidRequestDate,
     });
   },
 });
