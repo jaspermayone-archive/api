abfd0ec742fb25250c98252136bb53c67e9fea3a
Jasper Mayone <jasper@jaspermayone.com>
2022-09-08 17:52:41 -0400

    :zap: Hotfix!

--- src/middleware/rateLimitController.ts
+++ src/middleware/rateLimitController.ts
@@ -6,8 +6,7 @@ const limiter = rateLimit({
   // set windowMs to 1 minute
   windowMs: 1 * 60 * 1000,
   max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-  message:
-    "You have exceeded the max requests per 15 minutes, please try again later.", // Message to send when max requests are exceeded
+  message: "You have exceeded the max requests, please try again later.", // Message to send when max requests are exceeded
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
