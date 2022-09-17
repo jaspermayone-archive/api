c6ba20745e65a51a2eb3c5ca9092e555356001af
Satyabrat Panda <satyabratp.m.2000@gmail.com>
2022-09-17 22:00:26 +0530

    admin can access everyone's data, a user can access it's own data

--- src/routes/api/metrics.ts
+++ src/routes/api/metrics.ts
@@ -1,49 +1,67 @@
 import express from "express";
+import { isAdminOrAccessingOwnData } from "../../middleware/isAdminOrAccessingOwnData";
 import { MetricsModel } from "../../models/Metrics";
 import getHistory from "../../utils/getHistory";
 import { getLocationDetails } from "../../utils/getLocationDetails";
 
 const router = express.Router();
 
-router.get("/location", async (req: any, res: any) => {
-  const latestLocation = getLocationDetails(req.ipinfo);
-  return res.status(200).json(latestLocation);
-});
+router.get(
+  "/location/:userId",
+  isAdminOrAccessingOwnData,
+  async (req: any, res: any) => {
+    const latestLocation = getLocationDetails(req.ipinfo);
+    return res.status(200).json(latestLocation);
+  }
+);
 
-router.get("/usage", async (req, res) => {
-  const queryResult = await MetricsModel.aggregate()
-    .sortByCount("urlAccessed")
-    .exec();
+router.get(
+  "/usage/:userId",
+  isAdminOrAccessingOwnData,
+  async (req: any, res) => {
+    const queryResult = await MetricsModel.aggregate([
+      {
+        $match: { id: req.user.userId },
+      },
+    ])
+      .sortByCount("urlAccessed")
+      .exec();
 
-  let totalApiCalls = 0;
-  const response = queryResult.map((entity) => {
-    totalApiCalls += entity.count;
-    return {
-      url: entity._id,
-      count: entity.count,
-    };
-  });
+    let totalApiCalls = 0;
+    const response = queryResult.map((entity) => {
+      totalApiCalls += entity.count;
+      return {
+        url: entity._id,
+        count: entity.count,
+      };
+    });
 
-  return res.status(200).json({
-    apiUsageInfo: response,
-    totalApiCalls: totalApiCalls,
-  });
-});
+    return res.status(200).json({
+      apiUsageInfo: response,
+      totalApiCalls: totalApiCalls,
+    });
+  }
+);
 
-router.get("/history", async (req, res) => {
-  const fromDateISO: string =
-    req.query.fromDate?.toString() ?? new Date().toISOString();
-  const fromDate = new Date(fromDateISO);
-  const limit = Math.min((req.query.limit ?? 10) as number, 10);
-  const queryResult = await MetricsModel.find({
-    dateCreated: { $lt: fromDate },
-  }).limit(limit);
+router.get(
+  "/history/:userId",
+  isAdminOrAccessingOwnData,
+  async (req: any, res) => {
+    const fromDateISO: string =
+      req.query.fromDate?.toString() ?? new Date().toISOString();
+    const fromDate = new Date(fromDateISO);
+    const limit = Math.min((req.query.limit ?? 10) as number, 10);
+    const queryResult = await MetricsModel.find({
+      id: req.user.userId,
+      dateCreated: { $lt: fromDate },
+    }).limit(limit);
 
-  const history = queryResult.map(getHistory);
+    const history = queryResult.map(getHistory);
 
-  return res.status(200).json({
-    history: history,
-  });
-});
+    return res.status(200).json({
+      history: history,
+    });
+  }
+);
 
 export default router;
