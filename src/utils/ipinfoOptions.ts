228ebad691c7e32ce5fe65d262ad99beddcb48df
Satyabrat Panda <satyabratp.m.2000@gmail.com>
2022-09-17 15:17:29 +0530

    ipinfoOptions updated

--- src/utils/ipinfoOptions.ts
+++ src/utils/ipinfoOptions.ts
@@ -4,6 +4,5 @@ export const ipinfoOptions = {
   token: process.env.IP_INFO_BEARER_TOKEN,
   cache: null, // TOOD: Set caching mechanism
   timeout: 5000,
-  //ipSelector: defaultIPSelector,
-  ipSelector: (req) => "49.207.228.33",
+  ipSelector: defaultIPSelector,
 };
