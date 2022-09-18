c256acd5f370585b8ee4d329c51e51121b6544e9
Jasper Mayone <jasper@jaspermayone.com>
2022-09-18 13:55:40 -0400

    :bug: Small fix

--- src/utils/ipinfoOptions.ts
+++
@@ -1,8 +0,0 @@
-import { defaultIPSelector } from "ipinfo-express";
-
-export const ipinfoOptions = {
-  token: process.env.IP_INFO_BEARER_TOKEN,
-  cache: null, // TOOD: Set caching mechanism
-  timeout: 5000,
-  ipSelector: defaultIPSelector,
-};
