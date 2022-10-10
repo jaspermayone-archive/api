d8cded4a5c671520242e8530b485b8dc2f8fe522
Evan <infinitywaffles@outlook.com>
2022-10-10 16:30:16 -0700

    Added changelog endpoint

---
+++ src/routes/api/changelog.ts
@@ -0,0 +1,77 @@
+import axios from "axios";
+import express from "express";
+
+const router = express.Router();
+
+interface Release {
+  body: string;
+  tag_name: string;
+  prerelease: boolean;
+  draft: boolean;
+}
+
+router.get("/", (req, res) => {
+  const repoOwner = 'Heptagram-Project';
+  const repoName = 'discord-bot';
+  const apiEndpoint = `https://api.github.com/repos/${repoOwner}/${repoName}/releases`
+
+  // Max amount of pages to check for a non draft or pre-release before stopping
+  const maxAttempts = 3;
+
+  const formatRelease = (releaseBody: string) => {
+    // Remove markdown from the changelog
+    const changelog = releaseBody
+      .replace(/(\*\*|\*|`)/gm, "")
+      .replace(/^#{1,6}/gm , "");
+
+    // Remove commits made by renovate
+    const filteredChangelog = changelog
+      .split("\n")
+      .filter((line) => !line.includes("@renovate"))
+      .join("\n");
+
+    // Remove redundant line breaks and links and @ symbols
+    return filteredChangelog
+      .replace(/in https?:\/\/.*/g, "")
+      .replace(/(\r)/gm, "")
+      .replace(/(@)/gm, "")
+      .trim();
+  }
+
+  const getLatestNonDraftRelease = (releases: Release[]) => (
+    releases.find((release) => (
+      !release.draft && !release.prerelease
+    ))
+  );
+
+  (async () => {
+    let foundRelease = false;
+
+    for (let index = 0; index < maxAttempts; index++) {
+      await axios.get(`${apiEndpoint}?page=${index + 1}`).then((response) => {
+        const latestRelease = getLatestNonDraftRelease(response.data);
+
+        if (latestRelease) {
+          foundRelease = true;
+
+          res.status(200).send({
+            change_log: formatRelease(latestRelease.body),
+            version: latestRelease.tag_name,
+          });
+        }
+      });
+
+      if (foundRelease) {
+        break;
+      }
+
+      if (index === maxAttempts - 1) {
+        throw new Error("No recent non draft or pre-release was found");
+      }
+    }
+  })().catch((error: Error) => {
+    throw new Error(error.message);
+  });
+});
+
+export default router;
