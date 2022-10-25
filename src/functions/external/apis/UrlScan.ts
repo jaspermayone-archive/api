e95d2fc1ac90da3bfc1de1c3d7df91d3491fdd1c
Jasper Mayone <jasper@jaspermayone.com>
2022-10-25 06:37:37 -0400

    :bug: API key issue

--- src/functions/external/apis/UrlScan.ts
+++ src/functions/external/apis/UrlScan.ts
@@ -7,21 +7,35 @@ export const UrlScan = async (
 ) => {
   if (check) {
     const checkSerch = await axios.get(
-      `https://urlscan.io/api/v1/search/?q=domain:${link}`
+      `https://urlscan.io/api/v1/search/?q=domain:${link}`,
+      {
+        headers: {
+          "API-Key": process.env.URLSCAN_API_KEY,
+        },
+      }
     );
 
     // check if the link is not already scanned
     if (checkSerch.data.results.length === 0) {
       const scan = await axios.post(`https://urlscan.io/api/v1/scan/`, {
-        url: link,
-        public: "on",
-        visibility: "public",
+        headers: {
+          "API-Key": process.env.URLSCAN_API_KEY,
+        },
+        data: {
+          url: link,
+          public: "on",
+        },
       });
 
       // wait 15 seconds for the scan to finish
       setTimeout(async () => {
         const scanResult = await axios.get(
-          `https://urlscan.io/api/v1/result/${scan.data.uuid}/`
+          `https://urlscan.io/api/v1/result/${scan.data.uuid}/`,
+          {
+            headers: {
+              "API-Key": process.env.URLSCAN_API_KEY,
+            },
+          }
         );
 
         if (scanResult.data.verdicts.malicious) {
@@ -35,7 +49,12 @@ export const UrlScan = async (
     // if the link is already scanned, check the result
     else {
       const scanResult = await axios.get(
-        `https://urlscan.io/api/v1/result/${checkSerch.data.results[0].task.uuid}/`
+        `https://urlscan.io/api/v1/result/${checkSerch.data.results[0].task.uuid}/`,
+        {
+          headers: {
+            "API-Key": process.env.URLSCAN_API_KEY,
+          },
+        }
       );
       if (scanResult.data.verdicts.malicious) {
         return true;
